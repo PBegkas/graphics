13d7516a2593ed3a125acf3db5af1436508ec14e
ppapahr <34003820+ppapahr@users.noreply.github.com>
2020-11-18 23:50:15 +0200

    Changed perspective and alpha value

--- graphics1/core.fs
+++ graphics1/core.fs
@@ -3,5 +3,5 @@ in vec4 fragmentColor;
 out vec4 color;
 void main(){
 	color = fragmentColor;
-	color.a = 0.5;
+	color.a = 0.4;
 }
--- graphics1/main.cpp
+++ graphics1/main.cpp
@@ -92,10 +92,11 @@ int main(void)
 
 	// Camera matrix
 	glm::mat4 View = glm::lookAt(
-		glm::vec3(0, 0, -310), // Camera is at (4,3,3), in World Space
+		glm::vec3(-15, 0, -310), // Camera is at (4,3,3), in World Space
 		glm::vec3(0, 0, 0), // and looks at the origin
 		glm::vec3(0, 1, 0)  // Head is up (set to 0,-1,0 to look upside-down)
 	);
+	View = glm::rotate(View, glm::radians(40.0f), glm::vec3(1.0, 0.0, 0.0));
 	// Model matrix : an identity matrix (model will be at the origin)
 	glm::mat4 Model = glm::mat4(1.0f);
 
