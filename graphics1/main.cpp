2e735de2f33a80a56054190d5016ac42198b5896
Panos <p.p.giannako@gmail.com>
2020-11-21 21:57:11 +0200

    Scrap code cleanup, comments added/altered/deleted

--- graphics1/main.cpp
+++ graphics1/main.cpp
@@ -1,4 +1,4 @@
-﻿// Include standard headers
+// Include standard headers
 #include <stdio.h>
 #include <stdlib.h>
 #include <string>
@@ -8,14 +8,13 @@
 #include <vector>
 
 //hello from iolkos
+//hello from NDG
 
 // random lib is used to create random colours for the scene cube
 #include <random>
 
-// Include GLEW
+// Include GLEW/GLFW
 #include <GL/glew.h>
-
-// Include GLFW
 #include <GLFW/glfw3.h>
 GLFWwindow* window;
 
@@ -26,7 +25,7 @@ GLFWwindow* window;
 #include <glm/gtx/transform.hpp>
 using namespace glm;
 
-//#include "shader.hpp"
+// shader files
 #include "shaders.hpp"
 
 
@@ -43,10 +42,10 @@ int main(void)
 	glfwWindowHint(GLFW_SAMPLES, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); //  MacOS compatibility
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	// Open a window and create its OpenGL context
+	// Window making process
 	window = glfwCreateWindow(600, 600, u8"Συγκρουόμενα", NULL, NULL);
 	if (window == NULL) {
 		fprintf(stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n");
@@ -65,7 +64,7 @@ int main(void)
 		return -1;
 	}
 
-	// Ensure we can capture the escape key being pressed below
+	// Escape key
 	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
 
 	// Black background
@@ -85,41 +84,28 @@ int main(void)
 	// Get a handle for our "MVP" uniform
 	GLuint MatrixID = glGetUniformLocation(programID, "MVP");
 
-	// Projection matrix : 45° Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
+	// Projection matrix : 45° Field of View, 1:1 ratio, display range : 0.1 unit <-> 500 units
 	glm::mat4 Projection = glm::perspective(glm::radians(45.0f), 1.0f / 1.0f, 0.1f, 500.0f);
-	// Or, for an ortho camera :
-	//glm::mat4 Projection = glm::ortho(-10.0f,10.0f,-10.0f,10.0f,0.0f,100.0f); // In world coordinates
-
+	
 	// Camera matrix
 	glm::mat4 View = glm::lookAt(
-		glm::vec3(-15, 0, -310), // Camera is at (4,3,3), in World Space
+		glm::vec3(-15, 0, -310), // Camera is at (-15, 0, -310) in World Space
 		glm::vec3(0, 0, 0), // and looks at the origin
-		glm::vec3(0, 1, 0)  // Head is up (set to 0,-1,0 to look upside-down)
+		glm::vec3(0, 1, 0)  // Head is up 
 	);
+
 	View = glm::rotate(View, glm::radians(40.0f), glm::vec3(1.0, 0.0, 0.0));
-	// Model matrix : an identity matrix (model will be at the origin)
+	// Our Model matrix
 	glm::mat4 Model = glm::mat4(1.0f);
 
 	// scale the model x50
-	//glm::mat4 scaleMatrix;
-	//scaleMatrix = glm::scale(scaleMatrix, glm::vec3(50.0f, 50.0f, 50.0f));
-	//glm::mat4 transformation;
-	
 	Model = glm::scale(Model, glm::vec3(50.0f, 50.0f, 50.0f));
 
 	// "move" the cube
-	//glm::mat4 translateMatrix;
-	//translateMatrix = glm::translate(translateMatrix, glm::vec3(1.0f, 1.0f, 1.0f));
-
 	Model = glm::translate(Model, glm::vec3(1.0f, 1.0f, 1.0f));
 
-	//Model = myMatrix * Model;
-
-	//Model = transformation * Model;
-
-	// Our ModelViewProjection : multiplication of our 3 matrices
-	glm::mat4 MVP = Projection * View */* transformation * translateMatrix  *  scaleMatrix */  Model; // Remember, matrix multiplication is the other way around
-
+	// Our ModelViewProjection matrix
+	glm::mat4 MVP = Projection * View */* transformation * translateMatrix  *  scaleMatrix */  Model;
 
 	static const GLfloat g_vertex_buffer_data[] = {
 	-1.0f,-1.0f,-1.0f, // triangle 1 : begin
@@ -127,7 +113,7 @@ int main(void)
 	-1.0f, 1.0f, 1.0f, // triangle 1 : end
 	1.0f, 1.0f,-1.0f, // triangle 2 : begin
 	-1.0f,-1.0f,-1.0f,
-	-1.0f, 1.0f,-1.0f, // triangle 2 : end
+	-1.0f, 1.0f,-1.0f, // triangle 2 : end, etc
 	1.0f,-1.0f, 1.0f,
 	-1.0f,-1.0f,-1.0f,
 	1.0f,-1.0f,-1.0f,
@@ -161,7 +147,6 @@ int main(void)
 	};
 
 	// random colour for the scene cube
-
 	std::random_device rd;  //Will be used to obtain a seed for the random number engine
 	std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
 	std::uniform_real_distribution<> dis(0.0, 1.0);
@@ -211,47 +196,6 @@ int main(void)
 	};
 
 
-	/* NEEDS clean up
-	static const GLfloat g_color_buffer_data[] = {
-	0.583f,  0.771f,  0.014f,
-	0.609f,  0.115f,  0.436f,
-	0.327f,  0.483f,  0.844f,
-	0.822f,  0.569f,  0.201f,
-	0.435f,  0.602f,  0.223f,
-	0.310f,  0.747f,  0.185f,
-	0.597f,  0.770f,  0.761f,
-	0.559f,  0.436f,  0.730f,
-	0.359f,  0.583f,  0.152f,
-	0.483f,  0.596f,  0.789f,
-	0.559f,  0.861f,  0.639f,
-	0.195f,  0.548f,  0.859f,
-	0.014f,  0.184f,  0.576f,
-	0.771f,  0.328f,  0.970f,
-	0.406f,  0.615f,  0.116f,
-	0.676f,  0.977f,  0.133f,
-	0.971f,  0.572f,  0.833f,
-	0.140f,  0.616f,  0.489f,
-	0.997f,  0.513f,  0.064f,
-	0.945f,  0.719f,  0.592f,
-	0.543f,  0.021f,  0.978f,
-	0.279f,  0.317f,  0.505f,
-	0.167f,  0.620f,  0.077f,
-	0.347f,  0.857f,  0.137f,
-	0.055f,  0.953f,  0.042f,
-	0.714f,  0.505f,  0.345f,
-	0.783f,  0.290f,  0.734f,
-	0.722f,  0.645f,  0.174f,
-	0.302f,  0.455f,  0.848f,
-	0.225f,  0.587f,  0.040f,
-	0.517f,  0.713f,  0.338f,
-	0.053f,  0.959f,  0.120f,
-	0.393f,  0.621f,  0.362f,
-	0.673f,  0.211f,  0.457f,
-	0.820f,  0.883f,  0.371f,
-	0.982f,  0.099f,  0.879f
-	};
-	*/
-
 	GLuint colorbuffer;
 	glGenBuffers(1, &colorbuffer);
 	glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
@@ -262,7 +206,7 @@ int main(void)
 	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
 
-	// enable blending used in transparency
+	// Enable blending used in transparency
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
@@ -273,33 +217,34 @@ int main(void)
 
 		glUseProgram(programID);
 
-		// Send our transformation to the currently bound shader, 
-		// in the "MVP" uniform
+		// Send our transformation, to the currently bound shader,  in the "MVP" uniform
 		glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
 
 		// 1st attribute buffer : vertices
 		glEnableVertexAttribArray(0);
 		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
 		glVertexAttribPointer(
-			0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+			0,                  // attribute 0
 			3,                  // size
 			GL_FLOAT,           // type
-			GL_FALSE,           // normalized?
+			GL_FALSE,           // not normalized
 			0,                  // stride
 			(void*)0            // array buffer offset
 		);
+
 		// 2nd attribute buffer : colors
 		glEnableVertexAttribArray(1);
 		glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
 		glVertexAttribPointer(
-			1,                                // attribute. No particular reason for 1, but must match the layout in the shader.
+			1,                                // attribute 1, matches the layout in the shader.
 			3,                                // size
 			GL_FLOAT,                         // type
-			GL_FALSE,                         // normalized?
+			GL_FALSE,                         // not normalized
 			0,                                // stride
 			(void*)0                          // array buffer offset
 		);
-		// Draw the triangle !
+
+		// Triangle drawing
 		glDrawArrays(GL_TRIANGLES, 0, 12*3); // Starting from vertex 0; 3 vertices total -> 1 triangle
 		glDisableVertexAttribArray(0);
 		glDisableVertexAttribArray(1);
@@ -308,7 +253,7 @@ int main(void)
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 
-	} // Check if the ESC key was pressed or the window was closed
+	} // ESC key/window closed check
 	while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS &&
 		glfwWindowShouldClose(window) == 0);
 
